663815f85dfc8c300ff0e3dc6a1cc497e50a2ae2
fernandocamargoti <fernando.camargo.ti@gmail.com>
2013-08-03 16:35:06 -0300

    OP-20 - Fixed the case that the WrapperCircularPagerAdapter was dividing by zero when there's no item.

--- android-projects/widgets/src/main/java/br/com/cybereagle/androidwidgets/adapter/WrapperCircularPagerAdapter.java
+++ android-projects/widgets/src/main/java/br/com/cybereagle/androidwidgets/adapter/WrapperCircularPagerAdapter.java
@@ -118,9 +118,15 @@ public class WrapperCircularPagerAdapter extends PagerAdapter {
         if(circularViewPager == null){
             throw new IllegalStateException("The WrapperCircularPagerAdapter isn't attached to a CircularViewPager");
         }
-        int currentVirtualItem = circularViewPager.getCurrentVirtualItem();
-        super.notifyDataSetChanged();
-        circularViewPager.setCurrentVirtualItem(currentVirtualItem, getOffsetAmount()+currentVirtualItem, false);
+        int currentVirtualItem;
+        if(getVirtualCount() > 0){
+            currentVirtualItem = circularViewPager.getCurrentVirtualItem();
+            super.notifyDataSetChanged();
+            circularViewPager.setCurrentVirtualItem(currentVirtualItem, getOffsetAmount()+currentVirtualItem, false);
+        }
+        else {
+            super.notifyDataSetChanged();
+        }
     }
 
     public PagerAdapter getAdapter() {
